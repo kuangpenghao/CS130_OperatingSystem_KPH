e8613fab65360525469e67561c9dccc6e1145ed1
kuangpenghao <2653292820@qq.com>
2024-09-08 16:17:28 +0800

    solve other problems of priority:priority-change and priority-preempt

--- threads/thread.c
+++ threads/thread.c
@@ -202,6 +202,7 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
+  thread_yield();
 
   return tid;
 }
@@ -354,6 +355,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  thread_yield();
 }
 
 /* Returns the current thread's priority. */
